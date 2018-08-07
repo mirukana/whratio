e7ef43d6bafb6bc62cac7ab7efcb5f644f9c4980
miruka <miruka@disroot.org>
2018-08-07 12:49:28 -0400

    Update version to v2.0.1

--- whratio/__about__.py
+++ whratio/__about__.py
@@ -4,8 +4,8 @@
 """<SHORTDESC>"""
 
 __pkg_name__ = "whratio"
-__version__  = "2.0.0"
-__status__ = "Production"
+__version__  = "2.0.1"
+__status__   = "Production"
 
 __author__   = "miruka"
 __email__    = "miruka@disroot.org"
